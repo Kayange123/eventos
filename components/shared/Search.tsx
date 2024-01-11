dbf08683b806eb002d5be3f382fdaf8d6825d2c5
Ayubu Kayange <kayangejr3@gmail.com>
2024-01-11 11:46:55 +0300

    search component

--- components/shared/Search.tsx
+++ components/shared/Search.tsx
@@ -1,10 +1,53 @@
 "use client";
 
-import { useState } from "react";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { Input } from "../ui/input";
+import { formUrlQuery, removeKeysFromQuery } from "@/lib/types";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const Search = ({ placeholder }: { placeholder?: string }) => {
   const [query, setQuery] = useState("");
-  return <div>Search</div>;
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  useEffect(() => {
+    const debauncedValue = setTimeout(() => {
+      let newUrl = "";
+      if (query) {
+        newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "query",
+          value: query,
+        });
+      } else {
+        newUrl = removeKeysFromQuery({
+          params: searchParams.toString(),
+          keysToRemove: ["query"],
+        });
+      }
+      router.push(newUrl, { scroll: false });
+    }, 300);
+
+    return () => clearTimeout(debauncedValue);
+  }, [query, searchParams, router]);
+
+  return (
+    <div className="flex-center min-h-[54px] w-full overflow-hidden rounded-md bg-grey-50 px-4 py-2">
+      <Image
+        src="assets/icons/search.svg"
+        alt="icon search"
+        width={24}
+        height={24}
+      />
+      <Input
+        placeholder={placeholder}
+        type="search"
+        onChange={(e) => setQuery(e.target.value)}
+        className="p-regular-16 border-0 bg-grey-50 outline-offset-0 placeholder:text-grey-500 focus:border-0 focus-visible:ring-0"
+      />
+    </div>
+  );
 };
 
 export default Search;
